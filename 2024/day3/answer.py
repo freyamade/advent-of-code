2f01a6dcd18171f6827da163f48b18cccca180a7
freyamade <freya.broderick@honu.ai>
2024-12-03 10:50:55 +0100

    2024.3

--- 2024/day3/answer.py
+++ 2024/day3/answer.py
@@ -1,12 +1,36 @@
+import re
+PART_1_PATTERN = re.compile(r'mul\((\d{1,3}),(\d{1,3})\)')
+PART_2_PATTERN = re.compile(r"(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))")
+
+
 def get_part_1_answer(input: str) -> int:
-    return 0
+    total = 0
+    for a, b in PART_1_PATTERN.findall(input):
+        total += int(a) * int(b)
+    return total
 
 
 def get_part_2_answer(input: str) -> int:
-    return 0
+    total = 0
+    enabled = True
+    for mul, do, dont in PART_2_PATTERN.findall(input):
+        if do:
+            enabled = True
+        if dont:
+            enabled = False
+        if mul:
+            if not enabled:
+                continue
+            match = PART_1_PATTERN.match(mul)
+            a = int(match.group(1))
+            b = int(match.group(2))
+            total += a * b
+    return total
 
 
 if __name__ == '__main__':
+    assert get_part_1_answer('xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))') == 161
+    assert get_part_2_answer("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))") == 48
     with open('input.txt') as f:
         message = f.read().strip()
     print(f'Answer for part 1: {get_part_1_answer(message)}')
