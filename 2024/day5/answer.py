d5f5204126138e5aae0838e03ee03063edb3eec6
freyamade <freya.broderick@honu.ai>
2024-12-09 19:03:15 +0100

    2024.5

--- 2024/day5/answer.py
+++ 2024/day5/answer.py
@@ -1,12 +1,60 @@
-def generate_order_mapping
+from collections import defaultdict
+
+
+def generate_order_mapping(input: str) -> dict[str, list[str]]:
+    order_mapping = defaultdict(list)
+    for line in input.split('\n'):
+        if '|' not in line:
+            continue
+        page, required = line.split('|')
+        order_mapping[page].append(required)
+    return order_mapping
+
+
+def is_valid(order_mapping: dict[str, list[str]], pages: list[str]) -> bool:
+    for index in range(len(pages)):
+        for back_index in (range(index - 1, -1, -1)):
+            if pages[back_index] in order_mapping[pages[index]]:
+                return False
+    return True
 
 
 def get_part_1_answer(input: str) -> int:
-    return 0
+    order_mapping = generate_order_mapping(input)
+    valid_updates = []
+    for line in input.split('\n'):
+        if ',' not in line:
+            continue
+        pages = line.split(',')
+        if is_valid(order_mapping, pages):
+            valid_updates.append(pages)
+    output = 0
+    for update in valid_updates:
+        output += int(update[len(update) // 2])
+    return output
 
 
 def get_part_2_answer(input: str) -> int:
-    return 0
+    order_mapping = generate_order_mapping(input)
+    invalid_updates = []
+    for line in input.split('\n'):
+        if ',' not in line:
+            continue
+        pages = line.split(',')
+        if not is_valid(order_mapping, pages):
+            invalid_updates.append(pages)
+    output = 0
+    for update in invalid_updates:
+        # Fix it first
+        while not is_valid(order_mapping, update):
+            for index in range(len(update)):
+                for back_index in (range(index - 1, -1, -1)):
+                    if update[back_index] in order_mapping[update[index]]:
+                        update[index], update[back_index] = update[back_index], update[index]
+                        break
+        print('fixed', update)
+        output += int(update[len(update) // 2])
+    return output
 
 
 if __name__ == '__main__':
@@ -39,6 +87,8 @@ if __name__ == '__main__':
 75,97,47,61,53
 61,13,29
 97,13,75,29,47""".strip()
+    assert get_part_1_answer(test_message) == 143
+    assert get_part_2_answer(test_message) == 123
     with open('input.txt') as f:
         message = f.read().strip()
     print(f'Answer for part 1: {get_part_1_answer(message)}')
